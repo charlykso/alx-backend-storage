30eac6d4475a56e8aa76ddf066a73fc106d33759
Ikenna <ikenna@kali.Ikenna-PC>
2023-04-08 22:11:59 +0100

    Added the readme file and MySQL advanced file

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- create a table users with some properties
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT, 
+    email VARCHAR(255) NOT NULL UNIQUE, 
+    name VARCHAR(255), 
+    PRIMARY KEY(id)
+);
---
+++ README.md
@@ -0,0 +1,3 @@
+# Alx-backend-storage projects
+
+##
