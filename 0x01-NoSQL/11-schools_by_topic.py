1438c1c3c7a9b20d75cf4524cf271f819634a78d
Ikenna <ikenna@kali.Ikenna-PC>
2023-04-12 03:37:04 +0100

    Where can I learn Python?

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that returns the list of school having a specific topic:
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    return the list of school having a specific topic
+    """
+    return mongo_collection.find({topic: "topic"})
